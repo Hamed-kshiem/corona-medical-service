93d83e7c260d915b44af4df0d0e8a45ae3208712
Hamed Kshiem <keshim95@yahoo.com>
2020-12-07 12:44:38 +0100

    CORS Enabled

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors();
+
   const options = new DocumentBuilder()
     .setTitle('CoronaMedicalService')
     .setDescription(
